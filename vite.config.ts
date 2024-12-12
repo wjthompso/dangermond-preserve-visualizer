6c58595661282a77f2627a6c1241ebd4c2689599
wjthompso <49210425+wjthompso@users.noreply.github.com>
2024-12-11 16:15:15 -0800

    (WIP) Setting up Github Pages Deploy

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
-})
+    base:
+        process.env.NODE_ENV === "production"
+            ? "/dangermond-preserve-visualizer/"
+            : "/",
+    plugins: [react()],
+});
