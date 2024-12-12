cd2eb60acd561d3bae7a1be9fac71dbdde71649f
wjthompso <49210425+wjthompso@users.noreply.github.com>
2024-12-11 17:03:57 -0800

    (WIP) Reverting base URL thing

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,10 @@ import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    base:
-        process.env.NODE_ENV === "production"
-            ? "/dangermond-preserve-visualizer/"
-            : "/",
+    // base:
+    //     process.env.NODE_ENV === "production"
+    //         ? "/dangermond-preserve-visualizer/"
+    //         : "/",
+    base: "/",
     plugins: [react()],
 });
